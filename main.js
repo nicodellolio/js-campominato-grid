e6fcddb2a037ededa16853ba41a8105244f1bb2c
nicodellolio <nico.dellolio18@gmail.com>
2024-02-28 15:30:47 +0100

    add boxes and numbers - seek help

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
+//creiamo una griglia di prato fiorito con all'interno dei quadrati con all'interno dei numeri progressivi da 1 a 100
 
-
+//dichiariamo quante celle vogliamo
 const maxCellNumber = 100
 
+//iteriamo attraverso il ciclo for le caselle all/interno del DOM
 for (let i = 0; i < maxCellNumber; i++) {
+
+    // nella stessa iterazione inseriamo sia i quadrati che i numeri all'interno
     const markup = '<div class="box">' + [i + 1] + '</div>'
-    markupElement = document.getElementById('container').insertAdjacentHTML('beforeend',markup);
+    markupElement = document.getElementById('container').insertAdjacentHTML('beforeend', markup);
 }
+
+const box = document.getElementsByClassName('box')
+// console.log(box);
+
+box.addEventListener('click', function() {
+    box.classList.add('clicked')
+    console.log('clicked');
+})
+
+
 
 
