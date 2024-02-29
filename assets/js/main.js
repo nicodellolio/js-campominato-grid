43ca5b15c064ca668b0a332fa3e30ce818533133
nicodellolio <nico.dellolio18@gmail.com>
2024-02-29 12:19:11 +0100

    fix to the js structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,13 @@
 
 //dichiariamo quante celle vogliamo
 
+const playButton = document.getElementById('playButton')
 let diffLevel = document.getElementById('difficulty').value;
 
-function selectFunction() {
+playButton.addEventListener ('click', function (e) {
+
+    e.preventDefault()
+
     const setSize = document.getElementById('container')
     diffLevel = document.getElementById('difficulty').value;
     console.log(diffLevel);
@@ -13,28 +17,18 @@ function selectFunction() {
         maxCellNumber = 49
         console.log(maxCellNumber);
         setSize.classList.add('easy-size');
-
-
+        
     } else if (diffLevel === '2') {
         maxCellNumber = 81
         console.log(maxCellNumber);
         setSize.classList.add('medium-size');
 
-    } else if (diffLevel === '3'){
+    } else if (diffLevel === '3') {
         maxCellNumber = 100
         console.log(maxCellNumber);
         setSize.classList.add('hard-size');
     }
 
-    return maxCellNumber.value;
-}
-
-maxCellNumber = selectFunction()
-console.log(maxCellNumber);
-
-
-
-function generateGrid() {
     const getPlay = document.querySelector('.play')
     getPlay.classList.add('play-none');
 
@@ -42,31 +36,22 @@ function generateGrid() {
     //iteriamo attraverso il ciclo for le caselle all/interno del DOM
     for (let i = 0; i < maxCellNumber; i++) {
 
-    // nella stessa iterazione inseriamo sia i quadrati che i numeri all'interno
-    const markup = '<div class="box" >' + [i + 1] + '</div>'
-    markupElement = document.getElementById('container').insertAdjacentHTML('beforeend', markup);
-}
-
-let boxes = document.getElementsByClassName('box');
-
-for (let i = 0; i < boxes.length; i++) {
-    const box = boxes[i];
-
-    box.addEventListener('click', function (){
-        box.classList.add('clicked');
-        console.log(`Box selected: ` + [i + 1]);
-    })
-}
-
-}
+        // nella stessa iterazione inseriamo sia i quadrati che i numeri all'interno
+        const markup = '<div class="box" >' + [i + 1] + '</div>'
+        markupElement = document.getElementById('container').insertAdjacentHTML('beforeend', markup);
 
 
+    }
 
+    let boxes = document.getElementsByClassName('box');
 
-//EASY PEASY LEMON SQUEEZY 🤩
+    for (let i = 0; i < boxes.length; i++) {
+        const box = boxes[i];
 
+        box.addEventListener('click', function () {
+            box.classList.add('clicked');
+            console.log(`Box selected: ` + [i + 1]);
+        })
+    }
 
-// BONUS
-
-
-
+})
--- readme.md
+++ readme.md
@@ -1,3 +1,20 @@
+# Consegna 2
+- Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: i funghi magici.
+    Attenzione: nella stessa cella può essere posizionato al massimo un fungo, perciò nell’array dei funghi non potranno esserci due numeri uguali.
+- In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati
+    - abbiamo calpestato una fungo
+    - la cella si colora di rosso e la partita termina.
+
+Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+# La partita termina quando il giocatore clicca su una fungo o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono funghi).
+
+- Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una fungo.
+
+
+
+
+
+
 # Consegna
 L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 Ogni cella ha un numero progressivo, da 1 a 100.
